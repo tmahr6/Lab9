23d4cd192fbcf1acd31b4a269e6731cd5f9ee47d
evkes <evkessler2002@gmail.com>
2023-03-13 23:01:15 -0400

    Added a Scanner to read in info

--- Lab9.java
+++ Lab9.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Lab9 {
     public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
         System.out.println("Happy Spring Break!");
+        scnr.nextLine();
+
     }
 }
